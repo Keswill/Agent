f898f121cda7a8e2791850dc0d5640bce9808ffb
Keswill <Keswill@users.noreply.github.com>
2026-04-28 14:25:44 +0800

    Revert "Add standalone Sagittarius sort pipeline package"
    This reverts commit e7944d194d5b38932495a7507100c8deb583b466.

--- ros_packages/sagittarius_sort_pipeline/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-setup_args = generate_distutils_setup(
-    packages=['sagittarius_sort_pipeline'],
-    package_dir={'': 'src'},
-)
-
-setup(**setup_args)
